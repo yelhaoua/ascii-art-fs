0048dcf35d9837da9d300b009207803c210d3863
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-14 11:05:54 +0100

    structure of ascii-art-fs

--- func/printsymbole.go
+++ func/printsymbole.go
@@ -1,43 +1,5 @@
 package asciiart
 
-import (
-	"fmt"
-	"strings"
-)
-
 func PrintSymbole(arr [][]string, woord string) {
-	if woord == "" {
-		return
-	}
 
-	str := strings.ReplaceAll(woord, `\n`, "\n")
-
-	if strings.Trim(str, "\n") == "" {
-
-		for i := 0; i < len(woord); i++ {
-			if woord[i] == '\\' {
-				fmt.Println()
-			}
-		}
-		return
-	}
-
-	words := strings.Split(str, "\n")
-	for _, val := range words {
-		if val == "" {
-			fmt.Println()
-			continue
-		} else {
-			for i := 0; i < 8; i++ {
-				for _, sVal := range val {
-					if sVal < ' ' || sVal > '~' {
-						continue
-					} else if sVal >= 0 && int(rune(sVal)-32) < len(arr) {
-						fmt.Print(arr[int(rune(sVal)-32)][i])
-					}
-				}
-				fmt.Println()
-			}
-		}
-	}
 }
