9ced3e481d54bb1745eda63b3a51c7cf2c524114
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-15 14:29:06 +0100

    Splite func

--- func/splite.go
+++ func/splite.go
@@ -1,5 +1,20 @@
 package asciiart
 
+import (
+	"os"
+	"strings"
+)
+
 func Splite(fileName string) [][]string {
-	return [][]string{}
+	var res [][]string
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return [][]string{}
+	}
+	all := strings.Split(string(data), "\n")
+
+	for i := 1; i+8 < len(all); i += 9 {
+		res = append(res, all[i:i+8])
+	}
+	return res
 }
