dd3bc64ed83cfae36f5ca6590fe6eb3dd02b312f
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-15 18:02:23 +0100

    Add some comments for clarification

--- func/splite.go
+++ func/splite.go
@@ -1,20 +1,29 @@
 package asciiart
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
 
 func Splite(fileName string) [][]string {
 	var res [][]string
-	data, err := os.ReadFile(fileName)
+	var all []string
+	// Define the path of the banners
+	data, err := os.ReadFile("./banners/" + fileName + ".txt")
 	if err != nil {
+		fmt.Println(err)
 		return [][]string{}
 	}
-	all := strings.Split(string(data), "\n")
 
+	// Split the file by \n
+	all = strings.Split(string(data), "\n")
+
+	// Make every array contain 8 characters
 	for i := 1; i+8 < len(all); i += 9 {
 		res = append(res, all[i:i+8])
 	}
+
+	fmt.Println(res)
 	return res
 }
