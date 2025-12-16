23aee5d7fa5f62cbdd4be7ad9ea5518bc6cb479b
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-16 13:07:09 +0100

    some change

--- main.go
+++ main.go
@@ -34,6 +34,22 @@ func main() {
 
 			return
 		}
+
+		if len(os.Args) == 3 {
+			// os.args[2] == te name of  banner
+			Content := asciiart.Splite(os.Args[2])
+			// os.args[1] == the user text
+			asciiart.PrintSymbole(Content, os.Args[1])
+		} else if len(os.Args) == 2 {
+			// os.args[2] == te name of  banner
+			Content := asciiart.Splite("standard")
+			// os.args[1] == the user text
+			asciiart.PrintSymbole(Content, os.Args[1])
+		} else {
+			printErr()
+			return
+
+		}
 	}
 }
 
