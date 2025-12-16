0423a1eb9161aa5469e69c832a0e19224ac5b30b
mbelhouss <arandomdude75@gmail.com>
2025-12-16 12:02:48 +0100

    banners

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	asciiart "asciiart/func"
+	"fmt"
 	"os"
+
+	asciiart "asciiart/func"
 )
 
 func main() {
-	
+
 	if len(os.Args) == 2 {
 		Content := asciiart.Splite("standard")
 		// os.args[1] == the user text
@@ -17,7 +19,24 @@ func main() {
 		// os.args[1] == the user text
 		asciiart.PrintSymbole(Content, os.Args[1])
 	} else {
-		return
-	}
+		if len(os.Args) == 3 {
+			// os.args[2] == te name of  banner
+			Content := asciiart.Splite(os.Args[2])
+			// os.args[1] == the user text
+			asciiart.PrintSymbole(Content, os.Args[1])
+		} else if len(os.Args) == 2 {
+			// os.args[2] == te name of  banner
+			Content := asciiart.Splite("standard")
+			// os.args[1] == the user text
+			asciiart.PrintSymbole(Content, os.Args[1])
+		} else {
+			printErr()
 
+			return
+		}
+	}
+}
+
+func printErr() {
+	fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
 }
